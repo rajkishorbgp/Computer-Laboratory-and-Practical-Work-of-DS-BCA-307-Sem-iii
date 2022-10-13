55e9294cd11c4feba47a46d00609d1317d92fa78
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-10-13 17:41:20 +0530

    #7 To multiply two matrix A and B.

--- 7_MultiplyTwoMatrix.cpp
+++ 7_MultiplyTwoMatrix.cpp
@@ -1,42 +1,45 @@
 /*
+            program: 7
         Write a program To multiply two matrix A and B.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int a[3][3],b[3][3],i,j,k,sum;
-    cout<<"Enter the element of 3X3 matrix A :"<<endl;
-    for ( i = 0; i < 3; i++)
+    int a[3][3], b[3][3], i, j, k, sum;
+    cout << "Enter the element of 3X3 matrix A :" << endl;
+    for (i = 0; i < 3; i++)
     {
-        for ( j = 0; j < 3; j++)
+        for (j = 0; j < 3; j++)
         {
-            cin>>a[i][j];
+            cin >> a[i][j];
         }
     }
-    cout<<endl<<"Enter the element of 3X3 matrix B :"<<endl;
-    for ( i = 0; i < 3; i++)
+    cout << endl
+         << "Enter the element of 3X3 matrix B :" << endl;
+    for (i = 0; i < 3; i++)
     {
-        for ( j = 0; j < 3; j++)
+        for (j = 0; j < 3; j++)
         {
-            cin>>b[i][j];
+            cin >> b[i][j];
         }
     }
-    cout<<endl<<"multiply of two matrix A and B: "<<endl;
-    for ( i = 0; i < 3; i++)
+    cout << endl
+         << "multiply of two matrix A and B: " << endl;
+    for (i = 0; i < 3; i++)
     {
-        for ( j = 0; j < 3; j++)
+        for (j = 0; j < 3; j++)
         {
-            sum=0;
-            for ( k = 0; k < 3; k++)
+            sum = 0;
+            for (k = 0; k < 3; k++)
             {
-               sum = sum+(a[i][k]*b[k][j]);
-            }           
-            cout<<sum<<" ";
+                sum = sum + (a[i][k] * b[k][j]);
+            }
+            cout << sum << " ";
         }
-        cout<<endl;
-    }   
+        cout << endl;
+    }
     return 0;
 }
 
@@ -56,5 +59,5 @@ int main()
     18 18 18
     27 27 27
     36 36 36
-    
+
 */
