e5849ac6ecbbf5e8c263ccafe3173b4a3f08b464
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-11-19 15:16:45 +0530

    Write a C program To delete an element from an array.

--- 5_deleteElementAnArray.c
+++ 5_deleteElementAnArray.c
@@ -1,31 +1,31 @@
 /*
                     program: 5
-    Write a C++ program To delete an element from an array.
+    Write a C program To delete an element from an array.
 */
 
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 void printArray(int *, int);
 void deleteArray(int *, int, int);
 int main()
 {
-    int *arr, size, position, i;
-    cout << "Enter the size of array: ";
-    cin >> size;
-    arr = new int[size];
-
-    cout << "Enter the " << size << " element: " << endl;
-    for (i = 0; i < size; i++) // insert element in array
+    int size, arr[100], i;
+    int position, value;
+    printf("How many elements do you want to put in the array: ");
+    scanf("%d", &size);
+    printf("Enter the %d element: ", size);
+    for (i = 0; i < size; i++)
     {
-        cin >> arr[i];
+        scanf("%d", &arr[i]);
     }
-
-    cout << "Element is present in the array" << endl;
+    printf("Element is present in the array\n");
     printArray(arr, size);
-    cout << "Enter the location you want to delete an array: ";
-    cin >> position;
-    deleteArray(arr, size, position);
+
+    printf("Enter the location where you want to delete an element: ");
+    scanf("%d", &position);
+
+    deleteArray(arr, size - 1, position);
     printArray(arr, size - 1);
+
     return 0;
 }
 
@@ -34,16 +34,26 @@ void printArray(int arr[], int size)
     int i;
     for (i = 0; i < size; i++)
     {
-        cout << arr[i] << " ";
+        printf("%d ", arr[i]);
     }
-    cout << endl;
+    printf("\n");
 }
 
 void deleteArray(int arr[], int size, int position)
 {
     int i;
-    for (i = position - 1; i < size - 1; i++)
+    for (i = position - 1; i < size; i++)
     {
         arr[i] = arr[i + 1];
     }
 }
+
+/*
+    Output:
+    How many elements do you want to put in the array: 6
+    Enter the 6 element: 1 2 3 4 5 6
+    Element is present in the array
+    1 2 3 4 5 6
+    Enter the location where you want to delete an element: 0
+    2 3 4 5 6
+*/
