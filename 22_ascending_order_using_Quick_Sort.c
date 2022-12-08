06e4d79f63821b4e84bbd680ae507588685df9d7
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-09 01:03:24 +0530

    Write a program to arrange the list of numbers in ascending order using Quick Sort.

---
+++ 22_ascending_order_using_Quick_Sort.c
@@ -0,0 +1,62 @@
+/*
+                        @Program: 22
+    Write a program to arrange the list of numbers in ascending order using Quick Sort.
+*/
+
+#include <stdio.h>
+
+void quickSort(int *, int, int);
+int partition(int *, int, int);
+
+int main()
+{
+    int i, size;
+    int arr[] = {7, 8, 9, 6, 5, 4, 3, 2, 1, 0, 3, 7, 3};
+    size = sizeof(arr) / sizeof(int);
+    quickSort(arr, 0, size - 1);
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+void quickSort(int arr[], int si, int ei)
+{
+    if (si >= ei)
+    {
+        return;
+    }
+    // last element
+    int pIdx = partition(arr, si, ei);
+    quickSort(arr, si, pIdx - 1); // left
+    quickSort(arr, pIdx + 1, ei); // right
+}
+
+int partition(int arr[], int si, int ei)
+{
+    int pivot = arr[ei];
+    int i = si - 1; // to make place for els smaller than pivot
+
+    for (int j = si; j < ei; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            // swap
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    i++;
+    int temp = pivot;
+    arr[ei] = arr[i];
+    arr[i] = temp;
+    return i;
+}
+
+/*
+ *   Output:
+ *   0 1 2 3 3 3 4 5 6 7 7 8 9
+ */
